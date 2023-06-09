5b35060326b5862e2a1d6b61480f7afa3f64ffeb
Aravindo Wingeier <wingeier@adobe.com>
2023-06-09 15:14:58 -0700

    v3

--- src/jsMain/eyes-from-top-v3.glsl
+++ src/jsMain/eyes-from-top-v3.glsl
@@ -13,18 +13,33 @@ struct MovingHeadParams {
 
 #define PI 3.14159265358979323846
 #define DISTANCE_FORWARD 500.0 // Inches
-#define DISTANCE_SIDEWAYS 200.0 // Inches to each side
+#define DISTANCE_SIDEWAYS 500.0 // Inches to each side
 
 uniform FixtureInfo fixtureInfo;
 uniform vec2 center; // @@XyPad
 
+vec2 rotateAroundPoint(vec2 point, vec2 pivot, float angle) {
+    // Translate to origin
+    point -= pivot;
+
+    // Rotate
+    float s = sin(angle);
+    float c = cos(angle);
+    mat2 m = mat2(c, -s, s, c);
+    point = m * point;
+
+    // Translate back
+    point += pivot;
+
+    return point;
+}
 
 
 // @param params moving-head-params
 void main(out MovingHeadParams params) {
-    // x is forward
-    // y is height
-    // z is sideways
+    // x is forward (+ is forward)
+    // y is height (+ is up)
+    // z is sideways (+ is right)
 
 
     vec3 targetPoint = vec3(
@@ -33,13 +48,23 @@ void main(out MovingHeadParams params) {
     center.x * DISTANCE_SIDEWAYS - fixtureInfo.position.z
     );
 
+    // fixed for testing
+    //targetPoint= vec3(100, 0, -300);
 
+    // rotate target into space in front of eye
+    vec3 pointInFrontOfEye;
+    pointInFrontOfEye.xz = rotateAroundPoint(targetPoint.xz, fixtureInfo.position.xz, -fixtureInfo.rotation.y);
+    pointInFrontOfEye.y = targetPoint.y;
 
+    // angle from pointInFrontOfEye to axis at x=0 y=0 z=?
+    params.pan =  1.0 * PI - atan(pointInFrontOfEye.z, fixtureInfo.position.y);
+    // 2.0 * PI +
 
-    params.pan = 1.0 * PI;
-    params.tilt = PI / 4.0;
+    // params.pan = 1.0 * PI;
+    // params.tilt = PI / 4.0;
+    params.tilt = center.y * 1.6;
 
-    params.colorWheel = targetPoint.z;
+    params.colorWheel = pointInFrontOfEye.z;
     params.dimmer = 1.;
 
 }
